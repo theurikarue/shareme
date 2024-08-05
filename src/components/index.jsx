79a7799e4c54d3664b35c7a71f0fec240b9e1a75
theurikarue <iambonitheuri@gmail.com>
2024-08-05 16:52:46 +0300

    refactor: add exports to index.jsx

---
+++ src/components/index.jsx
@@ -0,0 +1,8 @@
+export {default as Home} from './Home.jsx'
+export {default as Login} from './Login.jsx'
+export {default as Sidebar} from './Sidebar.jsx'
+export {default as UserProfile} from './UserProfile.jsx'
+
+
+
+
