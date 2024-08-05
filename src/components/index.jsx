cadf726030c4e50d84b259bf2396a93e71c9c595
theurikarue <iambonitheuri@gmail.com>
2024-08-05 17:39:05 +0300

    refactor: finish setting up Pins container

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -4,5 +4,12 @@ export {default as Sidebar} from './Sidebar.jsx'
 export {default as UserProfile} from './UserProfile.jsx'
 
 
+export {default as Navbar} from './Navbar.jsx'
+export {default as Feed} from './Feed.jsx'
+export {default as PinDetail} from './PinDetail.jsx'
+export {default as CreatePin} from './CreatePin.jsx'
+export {default as Search} from './Search.jsx'
+
+
 
 
